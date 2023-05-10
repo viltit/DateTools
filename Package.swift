55760d44c3bc87942c974167b2a5a3a70cd5a67a
Titus Villiger <titus.villiger@meteoblue.com>
2023-05-10 18:31:31 +0200

    Trying to update project to Package Manger 5.3

--- Package.swift
+++ Package.swift
@@ -3,8 +3,18 @@ import PackageDescription
 
 let package = Package(
     name: "DateToolsSwift",
+    products: [
+        .library(
+            name: "DateToolsSwift",
+            targets: [
+                "DateToolsSwift"
+            ]
+        )
+    ],
     targets: [
-        Target(name: "DateToolsSwift")
+        .target(
+            name: "DateToolsSwift",
+            path: "DateToolsSwift/DateTools"
+        )
     ]
 )
-package.exclude = ["DateTools", "Examples", "Tests", "DateToolsSwift/Examples"]
